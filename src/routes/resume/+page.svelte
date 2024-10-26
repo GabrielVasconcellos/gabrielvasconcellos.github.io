509e990d5b7f12a2d66a7418a76529274aaeade4
GabrielVasconcellos <gabrielluiz.neves@gmail.com>
2024-10-26 02:32:13 -0300

    add resume page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex gap-10 flex-col">
+    <a class="link link-hover link-accent text-xl" href="resume.pdf" target="_blank">
+        {"> Download resume as pdf <"}
+    </a>
+    <img src="resume_en.png" alt="Resume"/>
+</div>
