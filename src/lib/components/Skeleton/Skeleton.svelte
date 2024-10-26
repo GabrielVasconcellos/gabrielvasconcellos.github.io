b2b6882e839175e2f9f6761de3523ca987b1590d
GabrielVasconcellos <gabrielluiz.neves@gmail.com>
2024-10-26 02:52:34 -0300

    add skeleton for loading animation

---
+++ src/lib/components/Skeleton/Skeleton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let isLoading = true;
+
+    function onLoad() {
+        isLoading = false;
+    }
+
+    export let className: string = "";
+</script>
+
+{#if isLoading}
+<div class="skeleton {className}">
+    <slot onload={onLoad}></slot>
+</div>
+{:else}
+     <slot></slot>
+{/if}
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,12 @@
+<script>
+	import Skeleton from "$lib/components/Skeleton/Skeleton.svelte";
+</script>
+
 <div class="flex gap-10 flex-col">
     <a class="link link-hover link-accent text-xl" href="resume.pdf" target="_blank">
         {"> Download resume as pdf <"}
     </a>
-    <img src="resume_en.png" alt="Resume"/>
+    <Skeleton>
+        <img src="resume_en.png" alt="Resume"/>
+    </Skeleton>
 </div>
